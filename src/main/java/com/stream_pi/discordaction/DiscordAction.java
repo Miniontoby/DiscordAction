227e9919c0b71580b78219e3ae5303e35be9e570
Miniontoby <tobias.gaarenstroom@gmail.com>
2022-03-08 17:32:43 +0100

    I hope this works

--- src/main/java/com/stream_pi/discordaction/DiscordAction.java
+++ src/main/java/com/stream_pi/discordaction/DiscordAction.java
@@ -8,9 +8,9 @@ import com.stream_pi.util.alert.StreamPiAlert;
 import com.stream_pi.util.alert.StreamPiAlertType;
 import com.stream_pi.util.version.Version;
 
+import java.io.IOException;
+
 public class DiscordAction extends NormalAction {
-    Button newWebhookButton;
-    Label currentWebhook;
 
     public DiscordAction() {
         setName("Discord");
@@ -25,10 +25,18 @@ public class DiscordAction extends NormalAction {
     @Override
     public void initProperties() throws MinorException {
         Property webhookURLProperty = new StringProperty("webhook_url");
-        webhookURLProperty.setControlType(ControlType.STRING);
         webhookURLProperty.setDisplayName("Webhook URL");
 
-        addClientProperties(webhookURLProperty);
+        Property contentProperty = new StringProperty("content");
+        contentProperty.setDisplayName("Webhook Text Content");
+
+        Property usernameProperty = new StringProperty("username");
+        usernameProperty.setDisplayName("Webhook Username");
+
+        Property titleProperty = new StringProperty("title");
+        titleProperty.setDisplayName("Webhook Title");
+
+        addClientProperties(webhookURLProperty, contentProperty, usernameProperty, titleProperty);
     }
 
     @Override
@@ -37,11 +45,27 @@ public class DiscordAction extends NormalAction {
     }
 
     public void sendMessage() throws MinorException {
-        Property webhookURL = getClientProperties().getSingleProperty("file_location");
+        String webhookURL = getClientProperties().getSingleProperty("webhook_url").getStringValue();
+        String webhookContent = getClientProperties().getSingleProperty("content").getStringValue();
+        String webhookTitle = getClientProperties().getSingleProperty("title").getStringValue();
+        String webhookUser = getClientProperties().getSingleProperty("username").getStringValue();
 
-        if (webhookURL.getStringValue().isBlank()) {
-            throw new MinorException("No Discord Webhook URL specified!");
+        if (webhookURL.isBlank()) {
+            throwMinorException("No Discord Webhook URL specified!");
         }
 
+        DiscordWebhook webhook = new DiscordWebhook(webhookURL);
+	webhook.setAuthor(webhookUser);
+
+        DiscordWebhook.EmbedObject embed = new DiscordWebhook.EmbedObject();
+        embed.setTitle(webhookTitle);
+        embed.setDescription(webhookContent);
+
+        try {
+            webhook.addEmbed(embed);
+            webhook.execute();
+        } catch (IOException e) {
+            throwMinorException(e.getMessage());
+        }
     }
 }
